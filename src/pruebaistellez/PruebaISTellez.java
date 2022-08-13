84c1d2d36d86551df309fb65853a6ee30d888142
joann <joann@LAPTOP-CG7QIBL4>
2022-08-13 09:08:05 -0500

    Prueba IS Tellez

---
+++ src/pruebaistellez/PruebaISTellez.java
@@ -0,0 +1,14 @@
+
+package pruebaistellez;
+
+
+public class PruebaISTellez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba GITHUB Tellez");
+    }
+    
+}
